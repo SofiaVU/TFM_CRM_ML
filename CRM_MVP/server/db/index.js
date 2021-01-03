a79484713474ab47ea8a41a4e8a746c6ee517e1f
SofiaVU <v_u_sofia@back.europe-west2-a.c.valid-ship-265707.internal>
2021-01-03 19:35:35 +0000

    fix: add back cloud changes

--- CRM_MVP/server/db/index.js
+++ CRM_MVP/server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose
     //.connect('mongodb://127.0.0.1:27017/CRM_7nov', { useNewUrlParser: true }) 
-    .connect('mongodb://127.0.0.1:27017/BBDD_OnlineRetail', { useNewUrlParser: true }) // --> resultado de aplicar funcion js mydataset
+    .connect('mongodb://10.154.0.2:27017/BBDD_OnlineRetail', { useNewUrlParser: true }) // --> resultado de aplicar funcion js mydataset
     //.connect('mongodb://127.0.0.1:27017/Initial_DataSet_withNames', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
